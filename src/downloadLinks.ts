4597df63eeb24c41469674b2a12ffef5b92ee40a
impulse-star <81616418+impulse-star@users.noreply.github.com>
2023-11-06 23:03:51 -0700

    began work on downloading links

--- src/downloadLinks.ts
+++ src/downloadLinks.ts
@@ -1,14 +1,22 @@
 import fs = require('fs');
+import { DISCORD_LINK_REGEX } from './banana';
 
 console.log("Downloading links...");
 
 const fileString = fs.readFileSync('../links.txt', 'utf8');
 
 let totalFilesDownloaded = 0;
+let startLogging = false;
 
 for (let line of fileString.split('\n')) {
     // Get media.discordapp.net links or cdn.discordapp.com links and split on the ?, download the
     // file before that.
+    if (line === "Attachment Urls:\n") {
+        startLogging = true;
+    }
+    if (startLogging) {
+        // pass
+    }
 }
 
 console.log("Program Completed.");
